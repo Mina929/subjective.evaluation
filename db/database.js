6adae740587c17abe4a1340e6bdb7dc936122c4e
Amina Hamza <hamzaamina1992@gmail.com>
2026-09-18 01:45:11 +0100

    Add database configuration for deployment

---
+++ db/database.js
@@ -0,0 +1,25 @@
+//const mysql = require('mysql2');
+
+//const db = mysql.createPool({
+  //  host: 'localhost',
+    //user: 'root',
+    //password: '**********',   // Ton mot de passe MySQL ici
+    //database: 'evaluation'
+//});
+
+//module.exports = db;
+
+const mysql = require('mysql2');
+
+const db = mysql.createPool({
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+module.exports = db;
